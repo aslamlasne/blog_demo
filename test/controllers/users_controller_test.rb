bf96d1c33bee6584832965d7420d4f3de42de5ef
Aslam Lasne <aslamlasne@gmail.com>
2024-12-30 20:08:35 -0600

    fix for test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+    sign_in users(:one)
+  end
+
   test "should get profile" do
-    get users_profile_url
+    get user_profile_url(users(:one).id)
     assert_response :success
   end
 end
